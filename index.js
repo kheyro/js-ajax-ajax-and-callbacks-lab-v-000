f0ba165f2069c52c61509eadf86a533981c8582d
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-19 13:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 $(document).ready(function (){
+
+
+
 });
+
+function searchRepositories() {
+  let searchTerm = document.getElementById('searchTerms')
+  let url = `https://api.github.com/search/repositories?q=${searchTerm}`
+  $.get(url).done()
+}
+
+
+//https:\/\/api.github.com\/search\/repositories\?q=
