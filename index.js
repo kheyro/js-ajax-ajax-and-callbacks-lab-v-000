179b6acdd03fa5a67001612b56ddd4c9f154d715
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-19 13:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function searchRepositories() {
   let searchTerm = document.getElementById('searchTerms')
   let url = `https://api.github.com/search/repositories?q=${searchTerm}`
   $.get(url).done(function(data) {
-    console.log('Data ' + data);
+    console.log(data);
   }).fail(function(error) {
     // This is called when an error occurs
     console.log('Something went wrong: ' + error);
